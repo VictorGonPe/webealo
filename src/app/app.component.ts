d471452d4569ccc33819afff9eab4a3e1c5b84af
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-12 13:32:26 +0200

    Add card in the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from "./shared/ui/logo/logo.component";
 import { HeaderComponent } from "./core/layout/header/header.component";
+import { MainFormComponent } from './features/main-form/main-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LogoComponent, HeaderComponent],
+  imports: [RouterOutlet, LogoComponent, HeaderComponent, MainFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/ui/card/card.component.ts
+++ src/app/shared/ui/card/card.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { TitleCardComponent } from '../title-card/title-card.component';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone:true,
+  imports: [TitleCardComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() price!:number;
 }
