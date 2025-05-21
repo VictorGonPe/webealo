2c7e86ca5ff2cc9e86d90a13b0e57cef9bf85792
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-05-21 10:44:25 +0200

    feat: add not-found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { BudgetsListComponent } from './features/budgets-list/budgets-list.component';
 import { BudgetDetailComponent } from './features/budget-detail/budget-detail.component';
 import { HomeComponent } from './views/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'presupuesto/:id', component: BudgetDetailComponent }
+    { path: 'presupuesto/:id', component: BudgetDetailComponent },
+    { path: '**', component: NotFoundComponent }
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { MainFormComponent } from "../../features/main-form/main-form.component";
 import { BudgetsListComponent } from "../../features/budgets-list/budgets-list.component";
-import { LogoComponent } from "../../shared/ui/logo/logo.component";
 import { HeaderComponent } from "../../shared/layout/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [MainFormComponent, BudgetsListComponent, LogoComponent, HeaderComponent],
+  imports: [MainFormComponent, BudgetsListComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
